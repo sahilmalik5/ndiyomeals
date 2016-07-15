79119ea8c691fb68deb1b08c859e705258e685f1
Sahil Malik <sahil.malik@airtel.com>
2016-07-16 00:44:45 +0530

    ndiyomeals added

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://www.ndiyomeals.com/mobileapp/api",
+	'DialogDefaultTitle' : "Ndiyo Meals Restaurant Directory and Delivery Service",
+	'pushNotificationSenderid' : "379751914176",
+	'facebookAppId' : "582589401909414",
+	'APIHasKey' : ""
+};
